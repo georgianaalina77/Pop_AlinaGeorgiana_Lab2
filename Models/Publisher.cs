ea80f3e4893b0e169aa3563ff21b6fa3c4450671
ALINA-GEORGIANA POP <alina.georgiana.pop@stud.ubbcluj.ro>
2025-10-12 22:48:48 +0300

    Add project files.

---
+++ Models/Publisher.cs
@@ -0,0 +1,11 @@
+﻿namespace Pop_AlinaGeorgiana_Lab2.Models
+
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; } //navigation property
+
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Pop_AlinaGeorgiana_Lab2.Data;
+using Pop_AlinaGeorgiana_Lab2.Models;
+
+namespace Pop_AlinaGeorgiana_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Pop_AlinaGeorgiana_Lab2.Data.Pop_AlinaGeorgiana_Lab2Context _context;
+
+        public IndexModel(Pop_AlinaGeorgiana_Lab2.Data.Pop_AlinaGeorgiana_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
